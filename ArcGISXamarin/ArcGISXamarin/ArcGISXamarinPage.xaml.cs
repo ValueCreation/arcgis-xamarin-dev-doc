3462a88a1d8fb98aa12f50d2327d0ecb0df336a8
kamiya takahiro <kamiya@kamiya-no-MacBook-Air.local>
2017-03-16 02:04:43 +0900

    add hands-on ArcGISXamarin

---
+++ ArcGISXamarin/ArcGISXamarin/ArcGISXamarinPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using Xamarin.Forms;
+using Esri.ArcGISRuntime;
+using Esri.ArcGISRuntime.Mapping;
+using Esri.ArcGISRuntime.UI.Controls;
+using Esri.ArcGISRuntime.Data;
+using Esri.ArcGISRuntime.Geometry;
+using Esri.ArcGISRuntime.UI;
+using Esri.ArcGISRuntime.Symbology;
+using System;
+using System.Drawing;
+using System.Collections;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ArcGISXamarin
+{
+	public partial class ArcGISXamarinPage : ContentPage
+	{
+		public ArcGISXamarinPage()
+		{
+			InitializeComponent();
+
+			Initialize();
+		}
+
+		private async void Initialize()
+		{
+
+			// Web マップの URL を指定してマップを作成
+			var webMap = await Map.LoadFromUriAsync(new Uri("https://arcgis.com/home/item.html?id=9a6a1c9f857a4a68a6e405bb5917e620"));
+
+			// マップビューのマップに設定 
+			MyMapView.Map = webMap;
+
+			// マップがロードされた際の処理
+			if (MyMapView.Map.LoadStatus == LoadStatus.Loaded)
+			{
+				// マップビューのタップ イベントを登録
+				//MyMapView.GeoViewTapped += OnMapViewTapped;
+
+			}
+
+		}
+	}
+}
