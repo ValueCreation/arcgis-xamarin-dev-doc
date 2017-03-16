c3ea16e4e6319fffd0e266c224bc9c8786b665c9
kamiya takahiro <kamiya@kamiya-no-MacBook-Air.local>
2017-03-17 02:15:04 +0900

    空間検索の追加

--- ArcGISXamarin/ArcGISXamarin/ArcGISXamarinPage.xaml.cs
+++ ArcGISXamarin/ArcGISXamarin/ArcGISXamarinPage.xaml.cs
@@ -4,10 +4,12 @@ using Esri.ArcGISRuntime.Mapping;
 using Esri.ArcGISRuntime.Geometry;
 using Esri.ArcGISRuntime.Symbology;
 using Esri.ArcGISRuntime.UI;
+using Esri.ArcGISRuntime.Data;
 using Esri.ArcGISRuntime.Tasks.Geocoding;
 using System;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 
 #if WINDOWS_UWP
 using Colors = Windows.UI.Colors;
@@ -31,6 +33,10 @@ namespace ArcGISXamarin
 
 		//マップが操作可能であるかどうかを示す変数
 		private bool isMapReady;
+
+		private FeatureLayer myFeatureLayer;
+		private List<Feature> myFeatures = new List<Feature>();
+		private GraphicsOverlay myGraphicsOverlay = new GraphicsOverlay();
 
 
 		public ArcGISXamarinPage()
@@ -64,18 +70,15 @@ namespace ArcGISXamarin
 
 			isMapReady = true;
 
-			//await MyMapView.Map.LoadAsync();
-
 			// マップがロードされた際の処理
 			if (MyMapView.Map.LoadStatus == LoadStatus.Loaded)
 			{
-
-
-
-
 				// マップビューのタップ イベントを登録
-				//MyMapView.GeoViewTapped += OnMapViewTapped;
-
+				MyMapView.GeoViewTapped += OnMapViewTapped;
+				// Web マップに含まれる最上位のレイヤーを取得
+				myFeatureLayer = (FeatureLayer)MyMapView.Map.OperationalLayers[1];
+				// マップビューにグラフィック表示用のオーバレイを追加
+				MyMapView.GraphicsOverlays.Add(myGraphicsOverlay);
 			}
 
 		}
@@ -147,6 +150,67 @@ namespace ArcGISXamarin
 			SimpleRenderer resultRenderer = new SimpleRenderer() { Symbol = resultGeocoordingSymbol };
 
 			return resultRenderer;
+		}
+
+		private async void OnMapViewTapped(object sender, Esri.ArcGISRuntime.Xamarin.Forms.GeoViewInputEventArgs e)
+		{
+			try
+			{
+
+				Console.WriteLine("タップしました");
+
+				// 2回目以降の検索時でフィーチャが選択されている場合は、選択を解除
+				if (myFeatures != null && myFeatures.Count() != 0)
+				{
+					myFeatureLayer.UnselectFeatures(myFeatures);
+				}
+
+				// グラフィック オーバレイに追加したグラフィックを削除
+				myGraphicsOverlay.Graphics.Clear();
+
+				// タップした地点から250メートルのバッファーの円を作成し、グラフィックとして表示する
+				var buffer = GeometryEngine.Buffer(e.Location, 250);
+				var outLineSymbol = new SimpleLineSymbol(SimpleLineSymbolStyle.DashDot, System.Drawing.Color.Black, 2);
+				var fillSymbol = new SimpleFillSymbol(SimpleFillSymbolStyle.Null, System.Drawing.Color.White, outLineSymbol);
+				var graphic = new Graphic(buffer, null, fillSymbol);
+
+				myGraphicsOverlay.Graphics.Add(graphic);
+
+				// フィーチャの検索用のパラメーターを作成
+				var queryParams = new QueryParameters();
+				// 検索範囲を作成したバファーの円に指定
+				queryParams.Geometry = buffer;
+				// 検索範囲とフィーチャの空間的な関係性を指定（バファーの円の中にフィーチャが含まれる）
+				queryParams.SpatialRelationship = SpatialRelationship.Contains;
+				// フィーチャの検索を実行
+				FeatureQueryResult queryResult = await myFeatureLayer.FeatureTable.QueryFeaturesAsync(queryParams);
+
+				var alertString = "";
+
+				// 検索結果のフィーチャのリストを取得
+				myFeatures = queryResult.ToList();
+				// 検索結果のフィーチャを選択（ハイライト表示）
+				myFeatureLayer.SelectFeatures(myFeatures);
+
+				for (int i = 0; i < myFeatures.Count; ++i)
+				{
+					Feature feature = myFeatures[i];
+					// フィーチャの"Name"フィールドの属性値を取得
+					var nameStr = feature.GetAttributeValue("BuildingName");
+					alertString = alertString + Environment.NewLine + nameStr;
+					Console.WriteLine(nameStr);
+				}
+
+				// 取得した属性値をアラート表示
+				await DisplayAlert("検索結果", alertString, "OK");
+
+			}
+
+			catch (Exception ex)
+			{
+				await DisplayAlert("検索のエラー", ex.ToString(), "OK");
+			}
+
 		}
 
 	}
